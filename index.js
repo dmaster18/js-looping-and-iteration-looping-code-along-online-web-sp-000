d349a8d7cc17144ca9d190deef8eb5cfbc818c4b
David Strauch <dmaster18@gmail.com>
2020-08-12 17:56:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  for(let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return names;
+}
+
+
+function countDown(value) {
+  let i = 0;
+  while (value > i) {
+    console.log(i);
+    i--;
+  }
+}
