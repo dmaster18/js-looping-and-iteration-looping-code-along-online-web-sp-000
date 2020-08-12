c231cfa25a1b87b31bdabf91e59f7c0c289800da
David Strauch <dmaster18@gmail.com>
2020-08-12 17:59:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Code your solutions in this file
 function writeCards(names, event) {
+  let greeting_array = [];
   for(let i = 0; i < names.length; i++) {
-    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    greeting = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    console.log(greeting)
+    greeting_array.push(greeting)
   }
-  return names;
+  return greeting_array;
 }
 
 
